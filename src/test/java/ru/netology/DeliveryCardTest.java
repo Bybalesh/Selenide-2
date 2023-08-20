a18b11b97e9b74f3222d72c3383ebb1c02e485bd
Bybalesh <109616787+Bybalesh@users.noreply.github.com>
2023-08-20 18:07:04 +0600

    DeliveryCardTest.java

--- src/test/java/ru/netology/DeliveryCardTest.java
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -16,27 +16,26 @@ public class CardDelivery {
         return LocalDate.now().plusDays(addDays).format(DateTimerFormatter.ofPattern(pattern));
     }
 
-    @Test
-    public void ShouldBeSuccessCompleted(){
-        open("http://localhost:9999");
+   @Test
+    void shouldRegisterIfDateIsMoreThanOneYear() {
         $("[data-test-id=city] input").setValue("Во");
         $(byText("Волгоград")).click();
         $("[data-test-id=date] [value]").click();
         LocalDate dateDefault = LocalDate.now().plusDays(3);
-        LocalDate dateOfMeeting = LocalDate.now().plusDays(30);
+        LocalDate dateOfMeeting = LocalDate.now().plusYears(1);
         String stringToSearch = dateOfMeeting.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         String dayToSearch = String.valueOf(dateOfMeeting.getDayOfMonth());
         if (dateOfMeeting.getMonthValue()>dateDefault.getMonthValue()|dateOfMeeting.getYear()>dateDefault.getYear()){
-            $(".calendar__arrow_direction_right[data-step='1']").click();
+            $(".calendar__arrow_direction_right[data-step='12']").click();
         }
         $$("td.calendar__day").find(exactText(dayToSearch)).click();
-        $("[data-test-id='name'] input").setValue("Петров Петр Петрович");
-        $("[data-test-id='phone'] input").setValue("+78567324855");
-        $("[data-test-id='agreement']").click();
-        $("button.button").click();
-        $(".notification__content")
-            .shouldBe(Condition.visible, Duration.ofSeconds(15));
-            .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+        $("[name='name']").setValue("Иванов Иван");
+        $("[name='phone']").setValue("+71111111111");
+        $("[data-test-id=agreement]").click();
+        $("[class='button__text']").click();
+        $("[data-test-id=notification]").waitUntil(Condition.visible, 15000)
+                .shouldHave(exactText("Успешно! Встреча успешно забронирована на " + stringToSearch));
+
 
     }
 
