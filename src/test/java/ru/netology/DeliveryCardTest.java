c2b48de073a186b61cda09218f7425006016c5b4
Bybalesh <109616787+Bybalesh@users.noreply.github.com>
2023-08-20 18:27:31 +0600

    DeliveryCardTest.java

--- src/test/java/ru/netology/DeliveryCardTest.java
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -12,22 +12,36 @@ import static com.codeborne.selenide.Selenide.open;
 
 
 public class CardDelivery {
-    @BeforeEach
-    public void setUp() {
-        open("http://localhost:9999");
-    }
+   
     private String generateDate(int addDays, String pattern){
         return LocalDate.now().plusDays(addDays).format(DateTimerFormatter.ofPattern(pattern));
     }
 
     @Test
-    public void ShouldBeSuccessCompleted(){
+    public void ShouldBeSuccessCityCompleted(){
+        open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Во");
         $(byText("Волгоград")).click();
+        String currentDate = generateDate(4,"dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(currentDate);
+        $("[data-test-id='name'] input").setValue("Петров Петр Петрович");
+        $("[data-test-id='phone'] input").setValue("+78567324855");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+            .shouldBe(Condition.visible, Duration.ofSeconds(15));
+            .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+
+    }
+    @Test
+    public void ShouldBeSuccessTimeCompleted(){
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Волгоград");
         
-        LocalDate defaultDay = LocalDate.now().plusDays(3);
-        LocalDate planDay = LocalDate.now().plusDays(30);
-        String str = LocalDate.now().plusDays(30).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        LocalDate defaultDay = LocalDate.now().plusDays(AddDays);
+        LocalDate planDay = LocalDate.now().plusDays(AddDays);
+        String currentDate = LocalDate.now().plusDays(AddDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id=date] [value]").click();
         if ((planDay.getYear() > defaultDay.getYear() | planDay.getMonthValue() > defaultDay.getMonthValue())) {
             $(".calendar__arrow_direction_right[data-step='1']").click();
@@ -41,7 +55,7 @@ public class CardDelivery {
         $("button.button").click();
         $(".notification__content")
             .shouldBe(Condition.visible, Duration.ofSeconds(15));
-            .shouldHave(Condition.exactText("Встреча успешно забронирована на " + str));
+            .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
 
     }
 
